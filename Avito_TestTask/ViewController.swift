314c0410232c9966cfd634aece3c2a25f5b0c341
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-09-25 12:34:23 +0300

    Стартовый коммит: - Удален сториборд, верстка кодом. Проект подготовлен для начала работы

--- Avito_TestTask/ViewController.swift
+++ Avito_TestTask/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Avito_TestTask
-//
-//  Created by Павел Афанасьев on 25.09.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
-
